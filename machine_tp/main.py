f51689b4dc5c7b3e431b39b7e4318651543632a6
bbustelo23 <bbustelo@mail.austral.edu.ar>
2025-09-15 16:16:27 -0300

    se me paso

--- machine_tp/main.py
+++ machine_tp/main.py
@@ -1,6 +1,6 @@
-from machine_tp.utiles.hu_moments_tp import generate_hu_moments_file
-from machine_tp.utiles.test_tp import load_and_test
-from machine_tp.utiles.entrenando_tp import train_model
+from utiles.hu_moments_tp import generate_hu_moments_file
+from utiles.test_tp import load_and_test
+from utiles.entrenando_tp import train_model
 
 generate_hu_moments_file()
 model = train_model()
